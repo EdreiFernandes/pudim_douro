d00b730a591f756be0eab989c971a837cd1181d9
Edrei Fernandes <34636871+EdreiFernandes@users.noreply.github.com>
2023-06-17 17:41:05 -0300

    Creating home screen (#2)
    * Creating home screen
    
    * Creating basic home screen
    
    * Creating widget ScoreTable
    
    * Renaming score table to scoreboard

---
+++ pudim_douro/lib/screens/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/scoreboard.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pudim DOuro'),
+        actions: const <Widget>[],
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 15,
+                ),
+                child: const Image(
+                  image: AssetImage('assets/images/logo_pudim.png'),
+                ),
+              ),
+            ],
+          ),
+          const Scoreboard(),
+        ],
+      ),
+    );
+  }
+}
