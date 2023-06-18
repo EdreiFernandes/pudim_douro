89722d93d8ef087edb8d45bbac4afd688e1bf329
Edrei Fernandes <34636871+EdreiFernandes@users.noreply.github.com>
2023-06-18 04:00:24 -0300

    Creating a summary list of recent years winners (#4)
    * Separating menu items
    
    * Setting menus navigation
    
    * Moving history from widget to screens
    
    * Creating summary list of recent years

--- pudim_douro/lib/widgets/side_menu.dart
+++ pudim_douro/lib/widgets/side_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pudim_douro/screens/home.dart';
 import 'package:pudim_douro/screens/login_screen.dart';
+import 'package:pudim_douro/screens/history.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({super.key});
@@ -10,44 +11,17 @@ class SideMenu extends StatelessWidget {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: [
-          const UserHeader(),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () => Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (context) => const Home(),
-              ),
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.create),
-            title: const Text('Placeholder'),
-            onTap: () => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const Placeholder(),
-              ),
-            ),
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text('Sair'),
-            onTap: () => Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (context) => const LoginScreen(),
-              ),
-            ),
-          ),
+        children: const [
+          MenuHeader(),
+          MenuItems(),
         ],
       ),
     );
   }
 }
 
-class UserHeader extends StatelessWidget {
-  const UserHeader({
+class MenuHeader extends StatelessWidget {
+  const MenuHeader({
     super.key,
   });
 
@@ -67,6 +41,48 @@ class UserHeader extends StatelessWidget {
           fit: BoxFit.cover,
         ),
       ),
+    );
+  }
+}
+
+class MenuItems extends StatelessWidget {
+  const MenuItems({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        ListTile(
+          leading: const Icon(Icons.home),
+          title: const Text('Home'),
+          onTap: () => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const Home(),
+            ),
+          ),
+        ),
+        ListTile(
+          leading: const Icon(Icons.history), //Icons.create
+          title: const Text('Histórico'),
+          onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const History(),
+              ),
+            ),
+        ),
+        const Divider(),
+        ListTile(
+          leading: const Icon(Icons.logout),
+          title: const Text('Sair'),
+          onTap: () => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const LoginScreen(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
