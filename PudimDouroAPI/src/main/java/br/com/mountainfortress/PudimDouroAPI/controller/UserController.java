0567ac83905d8ad61d2b088a8a6f47692f88811c
Edrei Fernandes <edreifernandes@gmail.com>
2023-07-07 22:03:23 -0300

    Testing api request

--- PudimDouroAPI/src/main/java/br/com/mountainfortress/PudimDouroAPI/controller/UserController.java
+++ PudimDouroAPI/src/main/java/br/com/mountainfortress/PudimDouroAPI/controller/UserController.java
@@ -1,5 +1,17 @@
 package br.com.mountainfortress.PudimDouroAPI.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> test(){
+        return ResponseEntity.ok("It Works");
+    }
+
 }
