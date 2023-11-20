11bba929714112be192e7d9a7b2cb3d15f78a987
Edrei Fernandes <edreifernandes@gmail.com>
2023-11-20 03:14:37 -0300

    Reading Session to fill user info on side_menu

--- pudim_douro/lib/widgets/side_menu.dart
+++ pudim_douro/lib/widgets/side_menu.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_session_manager/flutter_session_manager.dart';
+import 'package:pudim_douro/models/user.dart';
 import 'package:pudim_douro/screens/home.dart';
 import 'package:pudim_douro/screens/login_screen.dart';
 import 'package:pudim_douro/screens/history.dart';
 import 'package:pudim_douro/screens/vote.dart';
-
 
 class SideMenu extends StatelessWidget {
   const SideMenu({super.key});
@@ -27,24 +28,34 @@ class MenuHeader extends StatelessWidget {
     super.key,
   });
 
+  Future<User> _getloggedUser() async {
+    dynamic userJson = await SessionManager().get('user');
+    User user = User.fromJson(userJson);
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return UserAccountsDrawerHeader(
-      accountName: const Text('User Default'),
-      accountEmail: const Text('user@teste.com'),
-      currentAccountPicture: CircleAvatar(
-        child: ClipOval(
-          child: Image.asset('assets/images/user_default.png'),
-        ),
-      ),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/images/background_default.jpg'),
-          fit: BoxFit.cover,
-          opacity: 150,
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: _getloggedUser(),
+        builder: (context, loggedUser) {
+          return UserAccountsDrawerHeader(
+            accountName: Text(loggedUser.data?.name ?? ''),
+            accountEmail: Text(loggedUser.data?.email ?? ''),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.asset('assets/images/user_default.png'),
+              ),
+            ),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background_default.jpg'),
+                fit: BoxFit.cover,
+                opacity: 150,
+              ),
+            ),
+          );
+        });
   }
 }
 
@@ -52,6 +63,10 @@ class MenuItems extends StatelessWidget {
   const MenuItems({
     super.key,
   });
+
+  Future<void> _endSession() async {
+    await SessionManager().destroy();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,9 +104,10 @@ class MenuItems extends StatelessWidget {
           leading: const Icon(Icons.logout),
           title: const Text('Sair'),
           onTap: () => Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-              builder: (context) => LoginScreen(),
-            ),
+            MaterialPageRoute(builder: (context) {
+              _endSession();
+              return LoginScreen();
+            }),
           ),
         ),
       ],
