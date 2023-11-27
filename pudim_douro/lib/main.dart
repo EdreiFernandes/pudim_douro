06bc3ab139920d54f3d7170c9eb0c360ce7fa55d
Edrei Fernandes <34636871+EdreiFernandes@users.noreply.github.com>
2023-11-27 15:17:40 -0300

    Preparing v1 (#16)
    * Using pudim logo as icon
    
    * Adjusting app name

--- pudim_douro/lib/main.dart
+++ pudim_douro/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pudim D\'Ouro',
       theme: ThemeData.dark(),
       home: LoginScreen(),
     );
