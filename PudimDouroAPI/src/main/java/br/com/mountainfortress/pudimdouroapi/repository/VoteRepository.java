7cfefe7b60e5b19cff66c692b059ea667806d2cf
Edrei Fernandes <edreifernandes@gmail.com>
2023-11-27 16:30:40 -0300

    Correcting editionId parameter

--- PudimDouroAPI/src/main/java/br/com/mountainfortress/pudimdouroapi/repository/VoteRepository.java
+++ PudimDouroAPI/src/main/java/br/com/mountainfortress/pudimdouroapi/repository/VoteRepository.java
@@ -13,14 +13,14 @@ public interface VoteRepository extends JpaRepository<Vote, Integer> {
     String NEW_COUNTED_VOTE = "new br.com.mountainfortress.pudimdouroapi.model.CountedVote";
 
     @Query(value = "FROM Vote v WHERE v.edition = :edition_id and v.user = :user_id")
-    Vote findByEditionAndUser(@Param("edition_id") Integer edition_id, @Param("user_id") Integer userId);
+    Vote findByEditionAndUser(@Param("edition_id") Integer editionId, @Param("user_id") Integer userId);
 
     @Query(value = "SELECT " + NEW_COUNTED_VOTE + "(first_place, COUNT(first_place)) FROM Vote v WHERE v.edition = :edition_id GROUP BY first_place ORDER BY first_place")
-    List<CountedVote> countFirstPlaceInEdition(@Param("edition_id") Integer edition_id);
+    List<CountedVote> countFirstPlaceInEdition(@Param("edition_id") Integer editionId);
 
     @Query(value = "SELECT " + NEW_COUNTED_VOTE + "(second_place, COUNT(second_place)) FROM Vote v WHERE v.edition = :edition_id GROUP BY second_place ORDER BY second_place")
-    List<CountedVote> countSecondPlaceInEdition(@Param("edition_id") Integer edition_id);
+    List<CountedVote> countSecondPlaceInEdition(@Param("edition_id") Integer editionId);
 
     @Query(value = "SELECT " + NEW_COUNTED_VOTE + "(third_place, COUNT(third_place)) FROM Vote v WHERE v.edition = :edition_id GROUP BY third_place ORDER BY third_place")
-    List<CountedVote> countThirdPlaceInEdition(@Param("edition_id") Integer edition_id);
+    List<CountedVote> countThirdPlaceInEdition(@Param("edition_id") Integer editionId);
 }
