95637166cbdaf7e2babbd16ebd94673cf0afa042
Edrei Fernandes <edreifernandes@gmail.com>
2023-11-30 16:39:33 -0300

    Using nickname instead of name on scoreboard

--- PudimDouroAPI/src/main/java/br/com/mountainfortress/pudimdouroapi/repository/ScoreboardRepository.java
+++ PudimDouroAPI/src/main/java/br/com/mountainfortress/pudimdouroapi/repository/ScoreboardRepository.java
@@ -12,7 +12,7 @@ public interface ScoreboardRepository extends JpaRepository<Scoreboard, Integer>
     String NEW_SCOREBOARD_WITH_NAME = "new br.com.mountainfortress.pudimdouroapi.model.ScoreboardWithName";
 
     @Query(value = "SELECT " + NEW_SCOREBOARD_WITH_NAME +
-            "(s.id, u.name, s.gold_medal, s.silver_medal, s.brass_medal)" +
+            "(s.id, u.nickname, s.gold_medal, s.silver_medal, s.brass_medal)" +
             "FROM Scoreboard s INNER JOIN UserProfile u ON u.id = s.user_profile")
     List<ScoreboardWithName> findWithUserName();
 }
